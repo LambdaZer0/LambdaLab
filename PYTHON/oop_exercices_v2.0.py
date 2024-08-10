b1ed4f463e0e106b477846fa0ba86192bc3e54c5
LambdaZer0 <aboubakr.taleb.2@gmail.com>
2024-08-10 20:03:43 +0200

    Updated: 2024-08-10 20:02:07

--- PYTHON/oop_exercices_v2.0.py
+++ PYTHON/oop_exercices_v2.0.py
@@ -1,21 +1,7 @@
 
 """ Nouveau programme apprentissage OOP"""
 
-# Exercice 1:
-class Point2d:
-    """cette class defini un point de dimension 2 dans un espace euclidien elle comprend 3 attribus
-    -nom: intitule du point
-    -x: absisse
-    -y: ordonnee"""
-    def __init__(self, nom: str, x: float, y:float) -> None:
-        """constructeur de la classe Point2d"""
-        self.nom = nom
-        self.pos_x = x
-        self.pos_y = y
-
-# pt_a = Point2d("A", 2, 3)
-
-# Exercice 2:
+# serie 1:
 class Point2d:
     """cette class defini un point de dimension 2 dans un espace euclidien elle comprend 3 attribus
     -nom: intitule du point
@@ -28,34 +14,46 @@ class Point2d:
         self.pos_y = y
 
     def typeEtAdressMemoire(self) -> str:
-        ch = str(type(self))
-        ch += " à l'adresse "
-        ch += str(id(self))
-        return ch
-# pt_a = Point2d("A", 2, 3)
-# print(pt_a.typeEtAdressMemoire())
-
-# Exercice 3:
-class Point2d:
-    """cette class defini un point de dimension 2 dans un espace euclidien elle comprend 3 attribus
-    -nom: intitule du point
-    -x: absisse
-    -y: ordonnee"""
-    def __init__(self, nom: str, x: float, y:float) -> None:
-        """constructeur de la classe Point2d"""
-        self.nom = nom
-        self.pos_x = x
-        self.pos_y = y
-
-    def typeEtAdressMemoire(self) -> str:
+        """cette methode permer d afficher le type (type) et l adresse physique de la classe (id)"""
         ch = str(type(self))
         ch += " à l'adresse "
         ch += str(id(self))
         return ch
     
     def __repr__(self) -> str:
+        """methode speciale pour un affichage personnalise de la classe"""
         ch = f"{self.nom}({self.pos_x}, {self.pos_y})"
         return ch
+    
+    def getPosX(self) -> float:
+        """getter de la coordonnee x"""
+        return self.pos_x
+    
+    def setPosX(self, val) -> None:
+        """setter de la coordonnee x"""
+        if type(val) is not float:
+            raise TypeError("la coordonnee x doit etre de type float")
+        self.pos_x = val
+
+    def getPosY(self) -> float:
+        """getter de la coordonnee y"""
+        return self.pos_y
+    
+    def setPosY(self, val) -> None:
+        """setter de la coordonnee y"""
+        if type(val) is not float:
+            raise TypeError("la coordonnee y doit etre de type float")
+        self.pos_y = val
+
+    def getIntitule(self) -> str:
+        """getter du nom du point"""
+        return self.nom
+    
+    def setIntitule(self, val) -> None:
+        """setter du nom du point"""
+        if type(val) is not str:
+            raise TypeError("la coordonnee y doit etre de type string")
+        self.nom = val
 
 pt_a = Point2d("A", 2, 3)
 print("format personnalisé pour pt_a", pt_a)
