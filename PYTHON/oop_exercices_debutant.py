1d38d40237d32d6ed891f786464fa24f03038a44
LambdaZer0 <aboubakr.taleb.2@gmail.com>
2025-03-24 06:25:58 +0100

    Updated : 2025-03-24 06:25:41

--- PYTHON/oop_exercices_debutant.py
+++ PYTHON/oop_exercices_debutant.py
@@ -1,4 +1,5 @@
 """série d'exercices du livre '30 exercices en programmation orientée objet en python'"""
+from math import *
 
 #Exercice 1: création d'une classe simple, appel de méthodes
 class Biscuit:
@@ -54,10 +55,36 @@ class Note:
         else:
             print(f"""l'étudiant(e) {self.nomEtudiant} a échoué(e)""")
     
-note_1 = Note(80, "Julien")
-note_1.a_reussi()
-note_2 = Note(35, "Amélie")
-print(f"La note obtenue par {note_2.nomEtudiant} lors de la première correction est : {note_2.note}")
-note_2.note = 70
-print(f"Après une deuxième correction, la note d'{note_2.nomEtudiant} est égale à : {note_2.note}")
-note_2.a_reussi()
+# note_1 = Note(80, "Julien")
+# note_1.a_reussi()
+# note_2 = Note(35, "Amélie")
+# print(f"La note obtenue par {note_2.nomEtudiant} lors de la première correction est : {note_2.note}")
+# note_2.note = 70
+# print(f"Après une deuxième correction, la note d'{note_2.nomEtudiant} est égale à : {note_2.note}")
+# note_2.a_reussi()
+
+# Exercice 4: Création d'une classe contenant plusieurs méthodes
+class Cercle:
+    """attribus
+    -abscisse du centre x
+    -ordonnée du centre y
+    -rayon r"""
+    def __init__(self, x:float, y:float, r:float) -> None:
+        """initialistion des attribus"""
+        self.x = x 
+        self.y = y
+        self.r = r
+    def afficher_propriete(self) -> str:
+        return f"Le cerle est de rayon {self.r}cm, et de centre O({self.x}, {self.y})" 
+    def surface(self) -> float:
+        """calcul de la surface du cerle"""
+        return round(pi * self.r**2, 2)
+    def perimetre(self) -> float:
+        """calcul du périmètre du cerle"""
+        return round(2*pi*self.r, 2)
+    
+cercle_1 = Cercle(3, 4, 5)
+print(cercle_1.afficher_propriete())
+print(f"Le périmètre du cercle_1 est : {cercle_1.perimetre()}")
+print(f"La surface du cercle_1 est : {cercle_1.surface()}")
+    
