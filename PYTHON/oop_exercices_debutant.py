70f1a4d355d7c0ade0ef166f0c1e185facb99627
LambdaZer0 <aboubakr.taleb.2@gmail.com>
2025-04-28 14:43:41 +0200

    Updated: 2025-04-28 14:43:24

--- PYTHON/oop_exercices_debutant.py
+++ PYTHON/oop_exercices_debutant.py
@@ -240,9 +240,57 @@ class Employe:
         print(f"Le salarié {self.nom} travaille en tant que {self.fonction}.")
 
 employe_1 = Employe("Julien", "Ingénieur", 4000)
-employe_1.travailler(8)
-employe_1.info_salaire()
-employe_1.donner_augmentation(600)
-employe_1.info_fonction()
+# employe_1.travailler(8)
+# employe_1.info_salaire()
+# employe_1.donner_augmentation(600)
+# employe_1.info_fonction()
 
-
+# Exercice 11: Création d'une classe contenant plusieurs méthodes
+class Calcul_Numerique:
+    """ classe permettant d'effectuer plusieurs opération numériques:
+     attribut: nombre entier """
+    def __init__(self, nombre:int) -> None:
+        """ constructeur de la classe calcul_numerique """
+        self.nombre = nombre
+    
+    def calcul_factoriel(self) -> int:
+        """ calcul du factoriel du nombre """
+        return factorial(self.nombre)
+    
+    def liste_diviseurs(self) -> list:
+        """ calcul de la liste des diviseurs du nombre """
+        list_div = []
+        for i in range(1, self.nombre+1):
+            if self.nombre % i == 0:
+                list_div.append(i)
+        return list_div
+    
+    def estPremier(self) -> None:
+        """ methode pour vérifier si le nombre est premier """
+        if len(self.liste_diviseurs()) == 2:
+            print(f"Le nombre {self.nombre} est premier.")
+        else:
+            print(f"Le nombre {self.nombre} n'est pas premier.")
+    
+    def estPaire(self) -> None:
+        """ methode pour vérifier si le nombre est paire """
+        if self.nombre % 2 == 0:
+            print(f"Le nombre {self.nombre} est paire.")
+        else:
+            print(f"Le nombre {self.nombre} est impaire.")
+    
+premier_calcul = Calcul_Numerique(5)
+factoriel_5 = premier_calcul.calcul_factoriel()
+diviseurs_5 = premier_calcul.liste_diviseurs()
+print(f"Le factoriel du nombre {premier_calcul.nombre} est : {factoriel_5}")
+print(f"La liste des diviseurs du nombre {premier_calcul.nombre} est : {diviseurs_5}")
+premier_calcul.estPaire()
+premier_calcul.estPremier()
+print("\n")
+deuxieme_calcul = Calcul_Numerique(14)
+factoriel_5 = deuxieme_calcul.calcul_factoriel()
+diviseurs_5 = deuxieme_calcul.liste_diviseurs()
+print(f"Le factoriel du nombre {deuxieme_calcul.nombre} est : {factoriel_5}")
+print(f"La liste des diviseurs du nombre {deuxieme_calcul.nombre} est : {diviseurs_5}")
+deuxieme_calcul.estPaire()
+deuxieme_calcul.estPremier()
